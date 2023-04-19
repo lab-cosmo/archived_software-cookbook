f097b73734d8879b6b2eb082a30ebbb27591be24
Alexander Goscinski <alex.goscinski@posteo.de>
2023-04-19 11:39:37 +0200

    embedding widgets executed with jupyter-sphinx into sphinx-gallery

--- examples/mlp_models/linear_model.py
+++ examples/mlp_models/linear_model.py
@@ -20,9 +20,24 @@ Y = 0.1*X+10.1
 Y_p = Y + np.random.randn(10000)*1.5
 
 # %%
-# widget
+# widget does not work
 
 import ipywidgets
 from IPython.display import display
 widget = ipywidgets.IntSlider()
 display(widget)
+
+# %%
+# widget
+# ------
+# 
+# .. jupyter-execute::
+# 
+#     import ipywidgets as w
+#     from IPython.display import display
+# 
+#     a = w.IntSlider()
+#     b = w.IntText()
+#     w.jslink((a, 'value'), (b, 'value'))
+#     display(a, b)
+# 
