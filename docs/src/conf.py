60c40b773194dfa23262ae5ef8525d30cb06a1cd
Joseph Abbott <joseph.william.abbott@gmail.com>
2023-04-20 10:56:08 +0200

    Enable jupyter-sphinx

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -4,11 +4,18 @@ import os
 import re
 import time
 
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.todo',
-              'sphinx.ext.autosummary', 'sphinx.ext.extlinks',
-              'sphinx_gallery.gen_gallery',
-              'sphinx.ext.intersphinx',
-              'sphinx.ext.viewcode', 'sphinx.ext.inheritance_diagram']
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.doctest",
+    "sphinx.ext.todo",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.extlinks",
+    "sphinx_gallery.gen_gallery",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.inheritance_diagram",
+    "jupyter_sphinx",
+]
 
 example_subdirs = ["mlp_models", "sample_selection"]
 sphinx_gallery_conf = {
@@ -20,10 +27,10 @@ sphinx_gallery_conf = {
     "reference_url": {"cosmo-software-cookbook": None},
 }
 
-templates_path = ['_templates']
-exclude_patterns = ['_build']
+templates_path = ["_templates"]
+exclude_patterns = ["_build"]
 
-project = 'cosmo-software-cookbook'
-copyright = f'BSD 3-Clause License, Copyright (c) 2023, cosmo software cookbook team'
+project = "cosmo-software-cookbook"
+copyright = f"BSD 3-Clause License, Copyright (c) 2023, cosmo software cookbook team"
 
-htmlhelp_basename = 'cosmo-software-cookbook'
+htmlhelp_basename = "cosmo-software-cookbook"
